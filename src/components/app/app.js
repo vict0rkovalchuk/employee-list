3a554c17574cd1179ea64c96d1c1c9b5819bceff
Victor <vict02r05@gmail.com>
2023-04-10 11:42:27 +0300

    add key propert

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,9 +8,9 @@ import './app.css';
 
 function App() {
   const data = [
-    { name: 'John C.', salary: 800, increase: true },
-    { name: 'Alex M.', salary: 3000, increase: false },
-    { name: 'Carl W.', salary: 5000, increase: false }
+    { name: 'John C.', salary: 800, increase: true, key: 1 },
+    { name: 'Alex M.', salary: 3000, increase: false, key: 2 },
+    { name: 'Carl W.', salary: 5000, increase: false, key: 3 }
   ];
 
   return (
